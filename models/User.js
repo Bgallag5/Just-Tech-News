fc78a11914d7befcfe38372fd9d4a7fc027e3210
bengallagher <bgallag5@gmail.com>
2021-08-09 15:14:02 -0400

    hashed passwords

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model {}
@@ -45,6 +46,18 @@ User.init(
       }
     },
     {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+            },
+              // set up beforeUpdate lifecycle "hook" functionality
+  async beforeUpdate(updatedUserData) {
+    updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+    return updatedUserData;
+  }
+          },
       sequelize,
       timestamps: false,
       freezeTableName: true,
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -57,6 +57,7 @@ router.put('/:id', (req, res) => {
   
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
     User.update(req.body, {
+        individualHooks: true,
       where: {
         id: req.params.id
       }
